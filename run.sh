e9b217c25cc77367913830c4e82e6df8bfca0cd6
Anton Shyshko <pozernishku@gmail.com>
2021-02-01 07:01:33 +0200

    initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+raspivid -o - -t 0 -n -hf -fps 30 -b 4000000 \
+| ~/ffmpeg/ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le \
+-f s16le -ac 2 -i /dev/zero -f h264 \
+-i - -vcodec copy -acodec aac -ab 128k -g 50 \
+-strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/p26k-kec1-vehp-2f2a-392a
---
+++ vid.sh
@@ -0,0 +1,3 @@
+raspivid -o - -t 0 -op 100 -hf -fps 30 -b 6000000
+# -op 100
+# -n
