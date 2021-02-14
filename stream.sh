0c83f91a4b7cf9dfc447157b93cdadac631db8e3
Anton Shyshko <c-ashyshko@contentanalyticsinc.com>
2021-02-15 00:02:13 +0200

    add loglevel error

--- stream.sh
+++ stream.sh
@@ -1,7 +1,9 @@
 ffmpeg \
--re -stream_loop -1 \
+-loglevel error \
 -nostdin \
+-re -stream_loop -1 \
 -i video.mp4 \
 -c:v libx264 \
 -c:a aac \
--f fifo -fifo_format flv -map 0:v -map 0:a -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 "rtmp://a.rtmp.youtube.com/live2/${YT_API_CAMERA}"
+-f fifo -fifo_format \
+flv -map 0:v -map 0:a -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 "rtmp://a.rtmp.youtube.com/live2/${YT_API_CAMERA}"
