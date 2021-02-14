5c5d28986ec908127dcc6de15b6fabae6d1957b1
Anton Shyshko <c-ashyshko@contentanalyticsinc.com>
2021-02-14 04:38:33 +0200

    add separate file stream.sh

---
+++ stream.sh
@@ -0,0 +1,2 @@
+ffmpeg \
+-re -i video.mp4 -c:v libx264 -c:a aac -f fifo -fifo_format flv -map 0:v -map 0:a -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 "rtmp://a.rtmp.youtube.com/live2/${YT_API_CAMERA}"
