d32cc152722f6a8201373520a86d3a452bec11b8
Shyshko Anton <pozernishku@gmail.com>
2021-02-27 03:16:11 +0200

    clear/remove

--- stream_youtube_copy_file.sh
+++ stream_youtube_copy_file.sh
@@ -2,6 +2,6 @@ ffmpeg \
 -loglevel error \
 -nostdin \
 -re -stream_loop -1 \
--i video.mp4 \
+-i /home/pi/stream/video/video.mp4 \
 -c copy -preset superfast \
 -f flv -attempt_recovery 1 -recovery_wait_time 1 "rtmp://a.rtmp.youtube.com/live2/${YT_API_CAMERA}"
--- vid.sh
+++
@@ -1,3 +0,0 @@
-raspivid -o - -t 0 -op 100 -hf -fps 30 -b 6000000
-# -op 100
-# -n
